607b9e42ee4848bb251ba14b5e20d948704710de
Maxime Robion <max.robion@hey.com>
2023-08-04 10:15:50 +0200

    installing ngrx

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { register } from '../../store/actions';
+import { RegisterRequestInterface } from 'src/app/types/registerRequest.interface';
 
 @Component({
   selector: 'mc-register',
@@ -13,9 +16,13 @@ export class RegisterComponent {
         email: ['', Validators.required],
         password: ['', Validators.required],
     });
-    constructor(private fb:FormBuilder) {}
+    constructor(private fb:FormBuilder, private store: Store) {}
 
     onSubmit() {
         console.log('form', this.form.getRawValue());
+        const request: RegisterRequestInterface = {
+            user: this.form.getRawValue(),
+        };
+        this.store.dispatch(register({request}));
     }
 }

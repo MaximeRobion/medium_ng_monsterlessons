a9fa98f0913e442bb5d2f451072954626cb60a3b
Maxime Robion <max.robion@hey.com>
2023-08-09 11:55:32 +0200

    login ngrx before router

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -2,6 +2,7 @@ import { createActionGroup, props } from "@ngrx/store";
 import { RegisterRequestInterface } from "src/app/auth/types/registerRequest.interface";
 import { BackendErrorsInterface } from "src/app/shared/types/backendErrors.interface";
 import { CurrentUserInterface } from "src/app/shared/types/currentUser.interface";
+import { LoginRequestInterface } from "../types/loginRequest.interface";
 
 export const authActions = createActionGroup({
     source: 'auth',
@@ -9,5 +10,9 @@ export const authActions = createActionGroup({
         Register: props<{request: RegisterRequestInterface}>(),
         "Register Success": props<{currentUser: CurrentUserInterface}>(),
         "Register Failure": props<{errors: BackendErrorsInterface}>(),
+
+        Login: props<{request: LoginRequestInterface}>(),
+        "Login Success": props<{currentUser: CurrentUserInterface}>(),
+        "Login Failure": props<{errors: BackendErrorsInterface}>(),
     }
 });

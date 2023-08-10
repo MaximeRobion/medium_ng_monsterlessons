844b5e985883aefeabacad7154756d498818a2be
Maxime Robion <max.robion@hey.com>
2023-08-10 16:26:52 +0200

    feed: popular tag section

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,11 @@ import { provideHttpClient, withInterceptors } from "@angular/common/http";;
 import { provideEffects } from '@ngrx/effects';
 import * as authEffects from './app/auth/store/effects';
 import * as feedEffects from './app/shared/components/feed/store/effects';
+import * as popularTagsEffects from './app/shared/components/popularTags/store/effects';
 import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 import { authInterceptor } from "./app/shared/services/authInterceptor";
 import { feedFeatureKey, feedReducer } from "./app/shared/components/feed/store/reducer";
+import { popularTagsFeatureKey, popularTagsReducer } from "./app/shared/components/popularTags/store/reducers";
 
 bootstrapApplication(AppComponent, {providers: [
     provideHttpClient(withInterceptors([authInterceptor])),
@@ -20,7 +22,8 @@ bootstrapApplication(AppComponent, {providers: [
     provideStore({router: routerReducer}),
     provideState(authFeatureKey, authReducer),
     provideState(feedFeatureKey, feedReducer),
+    provideState(popularTagsFeatureKey, popularTagsReducer),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode(), autoPause: true, trace: false, traceLimit: 75 }),
-    provideEffects(authEffects, feedEffects),
+    provideEffects(authEffects, feedEffects, popularTagsEffects),
     provideRouterStore()
 ]});
